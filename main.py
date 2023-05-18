b0caa4f3fa17bfad6c2ce827a072beb134b62f77
chaturap <catur.ap@gmail.com>
2023-05-18 10:46:00 +0700

    coba get color on PDP, luama bgt prosesnya tapi g error dan belum berhasili

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ params = {
 
 }
 
+resultids = []
 result = []
 result_paging = []
 result_detail = []
+resultsize = []
 jumlahdata: int
 
 
@@ -59,7 +61,7 @@ def get_data(url: str):
     # item per page 48
     # lastpage 51
 
-    while page < int(lastpage)+1:
+    while page +48 < int(lastpage)+1:
         url=f"https://www.myer.com.au/c/men/mens-clothing/casual-shirts?pageNumber={page}"
         res = requests.get(url, params=params, headers=headers)
         if res.status_code == 200:
@@ -77,86 +79,139 @@ def get_data(url: str):
             for content in contents:
                 #  a.Necessary data: product’s name, id, brand, seoToken, variantData’s ids, stockIndicator, url
                 #  b.Nice to have dataSize guide id, size guide value
-                semua = content
-                id = content['id']
+                #semua = content
+                idproduct = content['id']
                 productName = content.find('span', attrs={'data-automation': 'product-name'}).text
                 brand = content.find('span', attrs={'data-automation': 'product-brand'}).text
                 price = content.find('span', attrs={'data-automation': 'product-price-was'}).text
-                linkUrl = url + content.find('a')['href']
+                #linkUrl = url + 'c/men/mens-clothing/casual-shirts?pageNumber=1/'+content.find('a')['href']
+                linkUrl = 'https://www.myer.com.au'+content.find('a')['href']
+
+                # Size guide id, size guide value
+                # https://www.myer.com.au/p/maddox-byson-long-sleeve-sherpa-lined-check-overshirt-in-black
+                urlsize = linkUrl
+                #urlsize = 'https://www.myer.com.au/p/levis-relaxed-fit-western-shirt-in-blue-white'
+
+
+                res_size = requests.get(urlsize, params=params, headers=headers)
+                soup_size = BeautifulSoup(res_size.text, 'html.parser')
+                #input_tags = soup_size.find_all('div', attrs={'data-automation': 'select-size'})
+                input_tags = soup_size.find_all('input', attrs={'name': 'size'})
+                ukuran = [tag['value'] for tag in input_tags]
+                #print(f'sizeid:{sizeId}')
+                color = soup_size.find('span', attrs={'data-automation': 'pdp-colour-display-value'}).text.strip()
+
+                size_guide = BeautifulSoup(res_size.text, 'html.parser')
+                table = size_guide.find_all('div', attrs={'class': 'sizeGuide'})
+                #print(f'table: {table}')
+
+
+                data_dict_size = {
+                    'id': idproduct,
+                    'size': ukuran,
+                    'color': color,
+                    #'table': table,
+
+
+                }
+
+                resultsize.append(data_dict_size)
+                try:
+                    os.mkdir('file_result')
+                except FileExistsError:
+                    pass
+                with open('file_result/final_datasize.json', 'w+') as json_datasize:
+                    json.dump(resultsize, json_datasize)
+                # print(f'Json Created page !!')
+                # jumlahdata = len(resultids)
+                # print(f"Total Item procesed : {jumlahdata}")
+
+                # create csv file
+                df = pd.DataFrame(resultsize)
+                df.to_csv('file_result/datasize.csv', index=False)
+                df.to_excel('file_result/datasize.xlsx', index=False)
+
+
+
+                # stock indicator
+                urlapi = f'https://api-online.myer.com.au/v2/product/productsupplemental?products={idproduct}'
+                r = requests.get(urlapi).json()
+                data = r['productList']
+
+                for d in data:
+                    id = d['id']
+                    internalId = d['internalId']
+                    stockIndicator = d['stockIndicator']
+                    #print(f'id={id} InternalId={internalId} StockIndicator={stockIndicator}')
+
+                # variantData’s ids
+                urlapi = f'https://api-online.myer.com.au/v2/product/pricesupplemental?products={idproduct}'
+                r = requests.get(urlapi).json()
+                data = r['productList']
+
+                for d in data:
+                    variants = d['variants']
+                    #print("variants : ", variants)
+
+                    for variant in variants:
+                        internalIds = variant['internalId']
+                        #print("internalIDS : ",internalIds)
+                        data_dict_ids = {
+                            'id': id,
+                            'internalIds': internalIds,
+                        }
+
+                        resultids.append(data_dict_ids)
+                        try:
+                            os.mkdir('file_result')
+                        except FileExistsError:
+                            pass
+                        with open('file_result/final_dataids.json', 'w+') as json_dataids:
+                            json.dump(resultids, json_dataids)
+                        # print(f'Json Created page !!')
+                        #jumlahdata = len(resultids)
+                        # print(f"Total Item procesed : {jumlahdata}")
+
+                        # create csv file
+                        df = pd.DataFrame(resultids)
+                        df.to_csv('file_result/dataids.csv', index=False)
+                        df.to_excel('file_result/dataids.xlsx', index=False)
+
+
+                    ###################################
+
+                data_dict = {
+                    'idproduct': idproduct,
+                    'name': productName,
+                    'brand': brand,
+                    'salesprices': price,
+                    'stockIndicator': stockIndicator,
+                    'linkUrl': linkUrl,
+                    'SeoToken': seotoken,
+                    #'SizeGuideId':size
+                    #'sizeGuideValue':sizeGuideValue
+                }
+
+                result.append(data_dict)
+                # print(f"result : {result}")
+
 
                 try:
-                    variantDatasids = soup.find('div', attrs={'data-bv-show': 'inline_rating'})['data-bv-product-id']
-                except:
-                    variantDatasids = ''
-                try:
-                    stockIndicator = content.find('div', attrs={'data-bv-show': 'inline_rating'})['data-bv-ready']
-                except:
-                    stockIndicator = ' '
+                    os.mkdir('file_result')
+                except FileExistsError:
+                    pass
+                with open('file_result/final_data.json', 'w+') as json_data:
+                    json.dump(result, json_data)
+                #print(f'Json Created page !!')
+                jumlahdata = len(result)
+                #print(f"Total Item procesed : {jumlahdata}")
 
-
-                #linkUrl = 'https://www.myer.com.au/p/maddox-sorrento-long-sleeve-sherpa-lined-check-overshirt-in-navy'
-                res_detail = requests.get(linkUrl, params=params, headers=headers)
-
-                #cek connection
-                #print(f"status: res_detail.status_code{res_detail.status_code}")
-
-                soup_detail = BeautifulSoup(res_detail.text, 'html.parser')
-                content_details = soup_detail.findAll('div', attrs={'id': 'add-to-bag'})
-                #print(f"content_details{content_details}")
-
-                for content_detail in content_details:
-                    color = content_detail.find('span', attrs={'data-automation': 'pdp-colour-display-value'}).text
-                    #SizeGuideId = content_detail.find('h2').text
-                    #sizeGuideValue = content_detail.find('table', attrs={'class': 'sizeGuideTable'}).text
-                    #print(f"color:{color}")
-                    #print(f"SizeGuideId:{SizeGuideId}")
-                    #print(f"sizeGuideValue:{sizeGuideValue}")
-
-                    # print(f"id: {id}")
-                    # print(f"productName: {productName}")
-                    # print(f"brand: {brand}")
-                    # print(f"price: {price}")
-                    # print(f"variantDatasids: {variantDatasids}")
-                    # print(f"SizeGuideId: {SizeGuideId}")
-                    # print(f"linkUrl: {linkUrl}")
-                    print(f"color: {color}")
-                    # print(f"semua: {semua}")
-                    # print(f"seo token : {seotoken}")
-
-                    data_dict = {
-                        'id': id,
-                        'name': productName,
-                        'brand': brand,
-                        'salesprices': price,
-                        #'variantDatasids': variantDatasids,
-                        'stockIndicator': stockIndicator,
-                        'linkUrl': linkUrl,
-                        'SeoToken': seotoken,
-                        #'color': color,
-                        #'SizeGuideId':SizeGuideId
-                        #'sizeGuideValue':sizeGuideValue
-                    }
-
-                    result.append(data_dict)
-                    # print(f"result : {result}")
-
-
-                    try:
-                        os.mkdir('file_result')
-                    except FileExistsError:
-                        pass
-                    with open('file_result/final_data.json', 'w+') as json_data:
-                        json.dump(result, json_data)
-                    #print(f'Json Created page !!')
-                    jumlahdata = len(result)
-                    #print(f"Total Item procesed : {jumlahdata}")
-
-                    # create csv file
-                    df = pd.DataFrame(result)
-                    df.to_csv('file_result/data.csv', index=False)
-                    df.to_excel('file_result/data.xlsx', index=False)
-                    # data created
-                    print(f"page {page} from {lastpage} Total Item procesed : {jumlahdata}")
+                # create csv file
+                df = pd.DataFrame(result)
+                df.to_csv('file_result/data.csv', index=False)
+                df.to_excel('file_result/data.xlsx', index=False)
+                # data created
+            print(f"page {page} from {lastpage} Total Item procesed : {jumlahdata}")
             page = page + 1
 
         else:
@@ -164,6 +219,9 @@ def get_data(url: str):
     print("File json csv and xlsx created success")
 
 
+
+
 if __name__ == '__main__':
     get_data(url)
+    #get_api(id)
     print("Scraping process is complete")
